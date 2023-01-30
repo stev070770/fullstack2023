9ca32573ff2365b95ba391dd71e44c518f7080e2
Jack <80575576+stev070770@users.noreply.github.com>
2023-01-30 12:59:11 +0100

    actualizacion

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Titular de la pagina</title>
+        <link rel="stylesheet" href="styles.css" />
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <h1>PRUEBA</h1>
+    </body>
+</html>
