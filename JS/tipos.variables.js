c841b38a8a2f62c3ef3a1e5ac2b5de96e17fcd27
Jack <80575576+stev070770@users.noreply.github.com>
2023-02-07 09:49:46 +0100

    Delete JS directory

--- JS/tipos.variables.js
+++
@@ -1,9 +0,0 @@
-const numero0 = 1;
-const numero11 = 20.1;
-const numero2 = 3.2e1;
-const numero3 = 3e2;
-const numero4 = 0x1f;
-const numero5 = 0b1010;
-const numero6 = 0o744;
-const numero7 = Number(2);
-console.log(const numero11)
--- JS/variables.js
+++
@@ -1,3 +0,0 @@
-var alert1 = "pepito";
-var alert2 = "HOLA"
-console.log(alert2)
