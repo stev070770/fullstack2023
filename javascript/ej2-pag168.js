126b4f3af986ba4ac55c4571254effc3558e2260
Jack <80575576+stev070770@users.noreply.github.com>
2023-02-08 10:57:23 +0100

    ejercicios pagina 168 sobre el if

---
+++ javascript/ej2-pag168.js
@@ -0,0 +1,36 @@
+//ejercicio2
+let1=2;
+let2=4;
+let3=6;
+let4=8;
+letsuma= let1 + let2;
+letsuma2=let3 + let4;
+if (letsuma>letsuma2) {
+    console.log(`a+b(${letsuma}) es mayor que c+d (${letsuma2})`);
+} else {
+    console.log(`a+b(${letsuma}) es menor que c+d (${letsuma2})`);
+}
+//ejercico3
+
+ang1=60;
+ang2=100;
+ang3=20;
+angtotal= ang1 + ang2 + ang3
+if (angtotal== 180) 
+{
+    document.write("La suma de los 3 angulos es igual a 180")
+} else {
+    document.write("La suma de los 3 angulos no suman 180")
+}
+//ejercicio4
+
+const number=2;
+const par=2%2;
+if (number%par)
+ { 
+    document.writer("Es par")   
+}
+ else 
+{ 
+    document.writer("No es par")
+}
