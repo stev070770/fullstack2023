3410ed96fe77001c31ef1a8e211106848d8f9f33
Jack <80575576+stev070770@users.noreply.github.com>
2023-02-08 14:43:20 +0100

    EJERCICIOS BUCLES pg171

---
+++ javascript/ejercicios-bucles.js
@@ -0,0 +1,15 @@
+//ejercicio 8
+for (let i = 51; i >=1; i--) {
+document.write(i)
+}
+//ejercicio9
+
+for (let i = 0; i<=10; i++) {
+    document.write("5 x" +i+"="+i*5+"<br>");
+}
+//ejercicio10
+let palabra = prompt("Dame una palabra");
+for (let i = 0; i < 100; i++) {
+    document.write(palabra+"<br>");
+    
+}
