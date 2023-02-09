e1d57ec328a84caab55e8eef1967cbd7eb03cec3
Jack <80575576+stev070770@users.noreply.github.com>
2023-02-09 14:05:12 +0100

    .

---
+++ javascript/string-pag-204.js
@@ -0,0 +1,29 @@
+let txt1="manera";
+let txt2="madre";
+if (txt1.charAt(0)=== txt2[0])
+ {
+    document.write("SI coincide la primera letra")}
+ else       
+ {
+    document.write("NO coincide la primera letra")
+}
+
+if (txt1.charAt(txt.lenght-1)===txt2[txt2.lenght-1]) {
+    document.write("y Si tiene la ultima letra" )
+} else {
+    document.write(",y No tiene la msima ultima letra")
+}
+
+//bucle: si las dos letras son iguales(text[]), entonces (if), si coinciden,(else) si no coinciden.
+//primero hacer variables de las palabras, despues utilizamos un bucle if con else, donde el la variable 1 con el charAt buscando la letra(0) primera letra , depsues opeador de que si es igual (===) de la variable2,si se cumple es SI coincide, y abajo en el else sea si no se cumple, NO coincide//
+
+
+///ejercicio3 pag 204
+
+let palabra= "hola";
+if (palabra.search(/[abc]/)=== palabra.chartAt(0));
+ {
+console.log("Tenemos un a, b o c delante o detras")
+} else<< {
+    console.log("No tenemos un a,b, o c delante o detras, palabra correcta")
+}
