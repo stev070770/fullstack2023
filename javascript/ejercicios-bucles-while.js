262f72b4fa42afbca7e79001660ce9e863850eb6
Jack <80575576+stev070770@users.noreply.github.com>
2023-02-09 11:27:26 +0100

    bucles,string,ejercicios

---
+++ javascript/ejercicios-bucles-while.js
@@ -0,0 +1,14 @@
+//ejercicio8 while
+let i=0
+while (i<=50) {
+    document.write(i);
+   i++;    
+}
+//ejercicio9 while
+let y = 0;
+while (y<=10) {
+    console.log("5 x" +y+"="+y*5+"<br>");
+    y++; 
+}
+//ejercicio11 while
+
