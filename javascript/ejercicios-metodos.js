13979149563df23a3946078a80aaacc399dac26f
Jack <80575576+stev070770@users.noreply.github.com>
2023-02-10 14:34:07 +0100

    metodos de array

--- javascript/ejercicios-metodos.js
+++ javascript/ejercicios-metodos.js
@@ -1,9 +1,29 @@
-//ejercicios1 pag212
-const edades=[10,11,12,13,14,15,16,17]
-for (let i = 0; i<edades.length; i++) {
-    console.log(edades[i]);
+//ejercicios3 pag212
+
+let colores= ["rojo",'azul','lila']
+let colores2=[];
+console.log(colores)
+console.log(colores2)
+
+
+ for (let color of colores) {
+    colores2.push(color)
+ }
+console.log(colores2)
+console.log(colores)
+console.log(colores.reverse())
+// .short
+
+const cosas = ['aula','carretera','bambu'];
+console.log(cosas.sort());
+console.log(cosas.reverse())
+// ejercicio4 pagina 212
+
+let libros= ["quijote","manolito","principito"]
+let escritos=["si","no","vale"]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];   
 }
-for (const edad of edades) {
-    console.log(edad);
-    
-}
+
+//objeto y fuction
